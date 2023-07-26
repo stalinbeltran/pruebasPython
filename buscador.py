64c4ef7211549b2804c1af58b8b09b33005e5b21
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-26 15:27:45 -0500

    encoding es utf-16le, funciona correctamente

--- buscador.py
+++ buscador.py
@@ -1,6 +1,6 @@
 
 def search_text_in_file(file_path, search_text):
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding='utf-16le') as file:
         lines = file.readlines()
     found_lines = []
     for line in lines:
@@ -15,8 +15,25 @@ def search_text_in_file(file_path, search_text):
 
 
 
+# def find_text_in_file(file_path, search_text):
+#     with open(file_path, 'r', encoding='utf-16') as file:
+#         for line_number, line in enumerate(file, 1):
+#             if search_text in line:
+#                 print(f"Found '{search_text}' on line {line_number}: {line.strip()}")
+
+# # Example usage:
+# file_path = './dump_SES.sql'
+# search_text = 'ddddd'
+
+# find_text_in_file(file_path, search_text)
+
+# exit(0)
+
+
+
+
 def save_text_to_file(file_path, lines):
-    with open(file_path, 'w') as file:
+    with open(file_path, 'w', encoding='utf-16le') as file:
         for line in lines:
             file.write(line + '\n')
 
@@ -28,7 +45,7 @@ file_path = './dump_SES.sql'
 # file_path = './pruebabuscar.sql'
 
 # search_text = 'pku-consultor@aig.gob.pa'
-search_text = u'Adjuntos'
+search_text = 'Adjuntos'
 found_lines = search_text_in_file(file_path, search_text)
 
 for line in found_lines:
