aebff4a5a7d22956f6b603da722602f08bf0bc41
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-27 15:31:48 -0500

    campos independientes

--- obtenerCampos.py
+++ obtenerCampos.py
@@ -5,7 +5,7 @@ import re
 
 
 #crear arreglos de tabla, y pares de campo/valor para determinar tabla y campos que contienen el texto buscado
-def camposValor(lineas):
+def getCampos(lineas):
     #[dbo].[Adjuntos]
     sql_sentence = "INSERT [dbo].[Adjuntos] ([id], [Solicitudes_id], [nombreArchivo], [envioID], [usuario], [fecha]) VALUES (9978, 1848, 'Observaciones_SES_1848.pdf', 'p1flp5sger16t11mtef3hovj1vr67', 'pku-consultor@aig.gob.pa', CAST('2021-11-30T14:42:58.000' AS DateTime))"
 
@@ -14,11 +14,15 @@ def camposValor(lineas):
     field_names = re.findall(r"INSERT.\[dbo\]\.\[Adjuntos\] \((.*?)\)", sql_sentence, re.DOTALL)
     print(field_names)
     if len(field_names) == 0: exit(0)
-    fields = field_names[0]
+    fields_string = field_names[0]
+    print(fields_string)
+    #[id], [Solicitudes_id], [nombreArchivo], [envioID], [usuario], [fecha]
+    fields = re.findall(r"\[(.*?)\]", fields_string, re.DOTALL)
+# .\[dbo\]\. 
     print(fields)
 
 
-camposValor(None)
+getCampos(None)
 
 exit(0)
 
