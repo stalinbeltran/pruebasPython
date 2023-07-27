88e91259fd899493ad8e2e7add8fb49c0ec71bc5
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-27 15:44:04 -0500

    obtenemos valores

--- obtenerCampos.py
+++ obtenerCampos.py
@@ -15,9 +15,20 @@ def getCampos(sql_sentence):
     fields = re.findall(r"\[(.*?)\]", fields_string, re.DOTALL)
     print(fields)
 
+#obtener valores de la sentencia SQL:
+def getValores(sql_sentence):
+    # Extract field VALUES using regex
+    field_names = re.findall(r"VALUES.\((.*?)\)", sql_sentence, re.DOTALL)
+    print(field_names)
+    if len(field_names) == 0: exit(0)
+    fields_string = field_names[0]
+    print(fields_string)
+    fields = fields_string.split(',')
+    for field in fields:
+        print(field)
 
 sql_sentence = "INSERT [dbo].[Adjuntos] ([id], [Solicitudes_id], [nombreArchivo], [envioID], [usuario], [fecha]) VALUES (9978, 1848, 'Observaciones_SES_1848.pdf', 'p1flp5sger16t11mtef3hovj1vr67', 'pku-consultor@aig.gob.pa', CAST('2021-11-30T14:42:58.000' AS DateTime))"
 
-getCampos(sql_sentence)
+getValores(sql_sentence)
 
 
