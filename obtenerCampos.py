4125c56c95436532b4ed287c328552acd04b2207
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-27 15:07:25 -0500

    funciona con dbo

--- obtenerCampos.py
+++ obtenerCampos.py
@@ -6,12 +6,12 @@ import re
 
 #crear arreglos de tabla, y pares de campo/valor para determinar tabla y campos que contienen el texto buscado
 def camposValor(lineas):
-    
-    sql_sentence = "INSERT  ([id], [Solicitudes_id], [nombreArchivo], [envioID], [usuario], [fecha]) VALUES (9978, 1848, 'Observaciones_SES_1848.pdf', 'p1flp5sger16t11mtef3hovj1vr67', 'pku-consultor@aig.gob.pa', CAST('2021-11-30T14:42:58.000' AS DateTime))"
+    #[dbo].[Adjuntos]
+    sql_sentence = "INSERT dbo ([id], [Solicitudes_id], [nombreArchivo], [envioID], [usuario], [fecha]) VALUES (9978, 1848, 'Observaciones_SES_1848.pdf', 'p1flp5sger16t11mtef3hovj1vr67', 'pku-consultor@aig.gob.pa', CAST('2021-11-30T14:42:58.000' AS DateTime))"
 
     # field_names = re.findall(r'\[([^\]]+)\]', sql_sentence)
     # Extract field names using regex
-    field_values = re.findall(r"INSERT. \((.*?)\)", sql_sentence, re.DOTALL)
+    field_values = re.findall(r"INSERT.dbo \((.*?)\)", sql_sentence, re.DOTALL)
     print(field_values)
 
 
