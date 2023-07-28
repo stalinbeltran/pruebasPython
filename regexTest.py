ab6084b65c6952e6a2373b03f4beb474df70b96a
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-28 10:31:32 -0500

    captura todo despues de ( menos un caracter antes del )

--- regexTest.py
+++ regexTest.py
@@ -4,7 +4,7 @@ sentence = "This [is] (some (nested text que eir between quien (external pare)nt
 
 # Extract text between the most external parentheses
 # pattern = r'(?<!\(.*)\((?!.*\))(.+?)(?<!\().*?\)(?!\))'
-pattern = r"\((?:.(?!\)))"
+pattern = r"\((?:.(?!\)))+"
 matches = re.findall(pattern, sentence)
 
 print(matches)
