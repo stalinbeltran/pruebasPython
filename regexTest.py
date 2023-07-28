1f0e25d79edebb1c0f20d4241497950f75077195
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-28 11:22:34 -0500

    obtiene todo entre parentesis, ignorando los intermedios

--- regexTest.py
+++ regexTest.py
@@ -4,7 +4,7 @@ sentence = "This [is] (some (nested text) que eir between quien (external parew)
 
 # Extract text between the most external parentheses
 # pattern = r'(?<!\(.*)\((?!.*\))(.+?)(?<!\().*?\)(?!\))'
-pattern = r"\((.)+\)"
+pattern = r"\([a-zA-Z\s\(\)]+\)"
 matches = re.findall(pattern, sentence)
 
 print(matches)
