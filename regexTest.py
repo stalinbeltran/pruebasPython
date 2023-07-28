1e77ce7d9e98dc0764c1dd9fc3b2373df1cf855b
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-28 10:30:14 -0500

    captura una letra depues de (

--- regexTest.py
+++ regexTest.py
@@ -1,10 +1,10 @@
 import re
 
-sentence = "This [is] (some (nested) text) que eir between quien (external) parentheses."
+sentence = "This [is] (some (nested text que eir between quien (external pare)ntheses"
 
 # Extract text between the most external parentheses
 # pattern = r'(?<!\(.*)\((?!.*\))(.+?)(?<!\().*?\)(?!\))'
-pattern = r'(?:.(?!\)))+$'
+pattern = r"\((?:.(?!\)))"
 matches = re.findall(pattern, sentence)
 
 print(matches)
