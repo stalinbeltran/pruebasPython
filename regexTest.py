ca8731a6ad6e710ae74854b3bba61550903ec121
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-28 11:10:51 -0500

    halla solo la w antes del )

--- regexTest.py
+++ regexTest.py
@@ -1,10 +1,10 @@
 import re
 
-sentence = "This [is] (some (nested text) que eir between quien (external pare)ntheses"
+sentence = "This [is] (some (nested text) que eir between quien (external parew)ntheses"
 
 # Extract text between the most external parentheses
 # pattern = r'(?<!\(.*)\((?!.*\))(.+?)(?<!\().*?\)(?!\))'
-pattern = r"\((?:.)*\)"
+pattern = r"\((.)+\)"
 matches = re.findall(pattern, sentence)
 
 print(matches)
