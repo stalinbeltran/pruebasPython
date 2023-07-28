01998f32c3e0b4dfb76607f30dca2749fd1b839e
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-07-28 10:59:48 -0500

    obtiene todo el texto desde el primer ( hasta el ultimo )

--- regexTest.py
+++ regexTest.py
@@ -1,10 +1,10 @@
 import re
 
-sentence = "This [is] (some (nested text que eir between quien (external pare)ntheses"
+sentence = "This [is] (some (nested text) que eir between quien (external pare)ntheses"
 
 # Extract text between the most external parentheses
 # pattern = r'(?<!\(.*)\((?!.*\))(.+?)(?<!\().*?\)(?!\))'
-pattern = r"\((?:.(?!\)))+"
+pattern = r"\((?:.)*\)"
 matches = re.findall(pattern, sentence)
 
 print(matches)
