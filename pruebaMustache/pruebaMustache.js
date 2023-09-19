78620277f35b38bce37f7ef6091061d06ca9b972
Stalin Beltran <stalin.beltran2006@gmail.com>
2023-09-19 09:57:16 -0500

    nombres apropiados

--- pruebaMustache/pruebaMustache.js
+++ pruebaMustache/pruebaMustache.js
@@ -9,18 +9,18 @@ function sumar(a, b){
 
   try {
     const json1 = fs.readFileSync('data1.json', 'utf8')
-    var viewjson = JSON.parse(json1)
+    var data1 = JSON.parse(json1)
 
     var partial = {}
 
     const funcionesRelacion = fs.readFileSync('funcionesRelacion.mustache', 'utf8')
     partial.funcionesRelacion = funcionesRelacion
 
-    console.log(viewjson)
+    console.log(data1)
 
-    fs.readFile('template1.php', 'utf8', function(err, data) {
+    fs.readFile('template1.php', 'utf8', function(err, template) {
         if (err) console.log(err)
-        var output = mustache.render(data, viewjson, partial);
+        var output = mustache.render(template, data1, partial);
         fs.writeFile('output.php', output, {options:'utf8'}, function(error){
 
         })
